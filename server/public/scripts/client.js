2b5fd5745bf6ccddbeaa47b9bdfadabc39ad26f5
Kevin Burk <percburk@gmail.com>
2021-01-08 17:43:28 -0600

    get and post routes functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,62 @@ $(onReady);
 
 function onReady() {
   console.log('JS JQ');
+  getTasks();
+  $('.inputBar').on('click', '#submitTask', sendNewTask);
 }
+
+function getTasks() {
+  $('#taskContainer').empty();
+  $.ajax({
+    type: 'GET',
+    url: '/todo',
+  })
+    .then(function (response) {
+      response.forEach((item) => {
+        $('#taskContainer').append(`
+        <div data-id="${item.id}">
+          <div>
+            <p>${item.task}</p>
+          </div>
+          <div>
+            <p>${item.due_date}</p>
+          </div>
+          <div>
+            <p>${item.priority}</p>
+          </div>
+          <div>
+            <p>Completed</p>
+          </div>
+          <div>
+            <p>Delete</p>
+          </div>
+        </div>
+      `);
+      });
+    })
+    .catch(function (error) {
+      console.log('error in get');
+    });
+} // end getTasks
+
+function sendNewTask() {
+  let newTask = {
+    task: $('#taskIn').val(),
+    due_date: $('#dueDate').val(),
+    priority: $('#priority').val(),
+    done: false,
+  };
+
+  $.ajax({
+    type: 'POST',
+    url: '/todo',
+    data: newTask,
+  }).then(function (response) {
+    console.log(response);
+    getTasks();
+    $('.inputBar input').val('');
+  }).catch(function (error) {
+    console.log('error in post', error);
+    alert('Error adding task.')
+  });
+} // end sendNewTask
--- server/routes/to-do-router.js
+++ server/routes/to-do-router.js
@@ -5,12 +5,53 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 // get route
+router.get('/', (req, res) => {
+  const queryText = `SELECT * FROM "to_do_list" ORDER BY "id"`;
+
+  pool
+    .query(queryText)
+    .then((result) => {
+      console.log(result.rows);
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('error in get', error);
+      res.sendStatus(500);
+    });
+});
 
 // post route
+router.post('/', (req, res) => {
+  let newTask = req.body;
+  console.log(newTask);
+
+  if (newTask.due_date === '') {
+    newTask.due_date = null;
+  }
+
+  const queryText = `
+    INSERT INTO "to_do_list" ("task", "due_date", "priority", "done")
+    VALUES ($1, $2, $3, $4);`;
+
+  pool
+    .query(queryText, [
+      newTask.task,
+      newTask.due_date,
+      newTask.priority,
+      newTask.done,
+    ])
+    .then((result) => {
+      console.log(result);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
 
 // put route
 
 // delete route
-
 
 module.exports = router;
